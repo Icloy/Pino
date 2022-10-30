7945e894f28962bacd7a5bfbbf3c7392b01e35cc
Icloy <70014902+Icloy@users.noreply.github.com>
2022-10-31 00:46:58 +0900

    커밋  v_0.0.01
    Inventory UI 베이스 추가
    prefabs 식물 등 추가
    floor material 변경

---
+++ Assets/02_Scripts/Player_Health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Health : MonoBehaviour
+{
+    //슬라이더를 받기
+    public Slider HungrySlider;
+    public Slider WaterSlider;
+    public Slider MentalSlider;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
