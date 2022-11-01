6aed861926d4e88d7c7218049c51743096a2ae7d
Icloy <70014902+Icloy@users.noreply.github.com>
2022-11-01 23:39:30 +0900

    커밋 v_0.0.03
    Game_Scene에서 옵션 버튼 활성화시 게임 정지 기능 추가

--- Assets/02_Scripts/Game_Option_menu.cs
+++ Assets/02_Scripts/Game_Option_menu.cs
@@ -9,6 +9,8 @@ public class Game_Option_menu : MonoBehaviour
     public GameObject OptionBtn;
     //게임종료 패널 오브젝트 가져오기
     public GameObject GameExitPanel;
+    //게임이 정지되었는지 확인하는 열거형 타입변수 제작
+    public bool isPause = false;
 
     //메인메뉴씬 불러오기
     public void GameOffBtn()
@@ -20,12 +22,14 @@ public class Game_Option_menu : MonoBehaviour
     public void OptionOn()
     {
         OptionBtn.SetActive(true);
+        pauseGame();
     }
 
     //옵션 버튼 다시 눌렀을 때 (끄기)
     public void OptionOff()
     {
         OptionBtn.SetActive(false);
+        pauseGame();
     }
 
     //게임종료패널 Yes버튼
@@ -38,5 +42,19 @@ public class Game_Option_menu : MonoBehaviour
     public void GameExitNo()
     {
         GameExitPanel.SetActive(false);
+    }
+
+    public void pauseGame()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
 }
