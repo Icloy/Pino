b0e65c07cba071613d2b24522ff6ef0955248e91
Yuhahak <dbwogkr1214@naver.com>
2022-11-30 15:52:46 +0900

    V_0.0.22
    1. 플레이어 모델링

--- Assets/02_Scripts/Player_Manager.cs
+++ Assets/02_Scripts/Player_Manager.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Move : MonoBehaviour
+public class Player_Manager : MonoBehaviour
 {
     public float speed;
     float hAxis;
     float vAxis;
-    float gravity = -20f; //중력
-    float yVelocity = 0f; //수직 속력
+    //float gravity = -20f; //중력
+    //float yVelocity = 0f; //수직 속력
 
     CharacterController cc;
     Vector3 moveVec;
 
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
     void Start()
     {
         cc = GetComponent<CharacterController>(); //캐릭터 컴포넌트 받아오기
@@ -26,9 +32,10 @@ public class Player_Move : MonoBehaviour
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        yVelocity += gravity * Time.deltaTime;
-        moveVec.y = yVelocity;
-
         cc.Move(moveVec * speed * Time.deltaTime);
+        
+        anim.SetBool("IdleToMove", moveVec != Vector3.zero);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
