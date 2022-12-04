62cab2b48cfbf82e44935c64dca8ae282d92838e
Icloy <70014902+Icloy@users.noreply.github.com>
2022-12-05 01:10:33 +0900

    V_0.0.28_2

---
+++ Assets/02_Scripts/Boss_Attack.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Attack : MonoBehaviour
+{
+    private Vector3 playertrans;
+
+    public float fireDamage;
+    public float fireSpeed;
+
+    private float curTime = 0f;
+    private float maxTime = 7f;
+
+    private void Awake()
+    {
+        playertrans = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(gameObject.transform.position, playertrans, fireSpeed);
+        curTime += Time.deltaTime;
+        if(curTime > maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player_Health.instance.IncDegHp("Hungry", -fireDamage);
+            Destroy(gameObject);
+        }
+    }
+}
